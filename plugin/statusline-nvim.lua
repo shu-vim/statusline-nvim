3e964446fe0bd050b07c41ff0de45d4df07fb4c1
Shuhei Kubota <kubota.shuhei+github@gmail.com>
2026-03-15 14:47:53 +0900

    autocmd in lua

--- plugin/statusline-nvim.lua
+++ plugin/statusline-nvim.lua
@@ -1,14 +1,26 @@
-vim.api.nvim_exec(
-  [[
-       augroup Statusline
-       au!
-       au WinEnter,BufEnter * set statusline=%!v:lua.require('statusline-nvim').active()
-       au WinLeave,BufLeave * setlocal statusline=%!v:lua.require('statusline-nvim').inactive()
-       au ColorScheme * lua require('statusline-nvim')._highlight(true)
-       augroup END
-     ]],
-  false
-)
+local group = vim.api.nvim_create_augroup('statusline-nvim', { clear = true })
+
+vim.api.nvim_create_autocmd({ 'WinEnter', 'BufEnter' }, {
+  group = group,
+  callback = function()
+    vim.o.statusline = "%!v:lua.require('statusline-nvim').active()"
+  end,
+})
+
+vim.api.nvim_create_autocmd({ 'WinLeave', 'BufLeave' }, {
+  group = group,
+  callback = function()
+    vim.wo.statusline = "%!v:lua.require('statusline-nvim').inactive()"
+  end,
+})
+
+vim.api.nvim_create_autocmd('ColorScheme', {
+  group = group,
+  callback = function()
+    require('statusline-nvim')._highlight(true)
+  end,
+})
+
 vim.o.showmode = false
 
 -- vim: set et ft=lua sts=2 sw=2 ts=2 :
