646101dfdeb447f70e972e2dbe2763f09d8e9f05
Shuhei Kubota <kubota.shuhei+github@gmail.com>
2026-03-15 17:08:03 +0900

    style: format

--- plugin/statusline-nvim.lua
+++ plugin/statusline-nvim.lua
@@ -2,23 +2,17 @@ local group = vim.api.nvim_create_augroup('statusline-nvim', { clear = true })
 
 vim.api.nvim_create_autocmd({ 'WinEnter', 'BufEnter' }, {
   group = group,
-  callback = function()
-    vim.o.statusline = "%!v:lua.require('statusline-nvim').active()"
-  end,
+  callback = function() vim.o.statusline = "%!v:lua.require('statusline-nvim').active()" end,
 })
 
 vim.api.nvim_create_autocmd({ 'WinLeave', 'BufLeave' }, {
   group = group,
-  callback = function()
-    vim.wo.statusline = "%!v:lua.require('statusline-nvim').inactive()"
-  end,
+  callback = function() vim.wo.statusline = "%!v:lua.require('statusline-nvim').inactive()" end,
 })
 
 vim.api.nvim_create_autocmd('ColorScheme', {
   group = group,
-  callback = function()
-    require('statusline-nvim')._highlight(true)
-  end,
+  callback = function() require('statusline-nvim')._highlight(true) end,
 })
 
 vim.o.showmode = false
