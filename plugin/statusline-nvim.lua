8f9df2a1ef02c37481d5089f50864999f0abb85c
Shuhei Kubota <kubota.shuhei+github@gmail.com>
2026-03-23 23:21:48 +0900

    fix: use vim.schedule to set vim.o.statusline without error E48

--- plugin/statusline-nvim.lua
+++ plugin/statusline-nvim.lua
@@ -3,9 +3,9 @@ local group = vim.api.nvim_create_augroup('statusline-nvim', { clear = true })
 vim.api.nvim_create_autocmd({ 'WinEnter', 'BufEnter', 'OptionSet', 'ModeChanged' }, {
   group = group,
   callback = function()
+    require('statusline-nvim')._dirty()
+    local statusline = "%!v:lua.require('statusline-nvim').active()"
     vim.schedule(function()
-      require('statusline-nvim')._dirty()
-      local statusline = "%!v:lua.require('statusline-nvim').active()"
       if vim.o.statusline ~= statusline then vim.o.statusline = statusline end
     end)
   end,
@@ -14,21 +14,17 @@ vim.api.nvim_create_autocmd({ 'WinEnter', 'BufEnter', 'OptionSet', 'ModeChanged'
 vim.api.nvim_create_autocmd({ 'WinLeave', 'BufLeave' }, {
   group = group,
   callback = function()
-    vim.schedule(function()
-      require('statusline-nvim')._dirty()
-      local statusline = "%!v:lua.require('statusline-nvim').inactive()"
-      if vim.wo.statusline ~= statusline then vim.wo.statusline = statusline end
-    end)
+    require('statusline-nvim')._dirty()
+    local statusline = "%!v:lua.require('statusline-nvim').inactive()"
+    if vim.wo.statusline ~= statusline then vim.wo.statusline = statusline end
   end,
 })
 
 vim.api.nvim_create_autocmd('ColorScheme', {
   group = group,
   callback = function()
-    vim.schedule(function()
-      require('statusline-nvim')._dirty()
-      require('statusline-nvim')._highlight(true)
-    end)
+    require('statusline-nvim')._dirty()
+    require('statusline-nvim')._highlight(true)
   end,
 })
 
